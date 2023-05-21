69e71f33f9128026f54cb7487e34cc7d2d651d2d
Vittorio Serra <ko92vuzu@multipro-r1p3.informatik.uni-erlangen.de>
2023-05-21 20:59:34 +0200

    instr mem and data mem done, integrating with pc and assembly missing

---
+++ ex4_mem/src/memories_and_pc.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/21/2023 08:19:31 PM
+-- Design Name: 
+-- Module Name: memories_and_pc - bh
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity memories_and_pc is
+--  Port ( );
+end memories_and_pc;
+
+architecture bh of memories_and_pc is
+
+begin
+
+
+end bh;
