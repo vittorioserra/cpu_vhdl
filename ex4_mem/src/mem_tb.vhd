8aed23f9e5840e02e9b03cc28aa3b384826abfd2
CeDoMain <CeDoMain@users.noreply.github.com>
2023-05-24 15:32:10 +0200

    merging and beautifying

--- ex4_mem/src/mem_tb.vhd
+++ ex4_mem/src/mem_tb.vhd
@@ -9,6 +9,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+
 library work;
 use work.utils.ALL;
 
@@ -19,16 +20,18 @@ architecture tb of mem_tb is
     constant CLOCK_PERIOD : time := 10 ns;
 	constant PORT_WIDTH : positive := 32;
 	constant BLOCK_COUNT : positive := 512;
-	constant MEM_INIT_FILE : string := "../src/fibonacci_code/fibonacci.o";
+	constant MEM_INIT_FILE : string := "../src/fibonacci_code/fibonacci_led.o";
 
 	signal clock : std_logic;
 	signal p1_enable, p2_enable, p2_write_enable : std_logic;
 	signal p1_addr, p2_addr : std_logic_vector(get_bit_count(BLOCK_COUNT) - 1 downto 0);
 	signal p2_val_in : std_logic_vector(PORT_WIDTH - 1 downto 0);
 	signal p1_val_out, p2_val_out : std_logic_vector(PORT_WIDTH - 1 downto 0);
+	
+	signal reset_n_pc : std_logic;
+	signal pc_value_ext : std_logic_vector(PORT_WIDTH - 1 downto 0);
 begin
-
-    DUT : entity work.mem 
+    DUT_mem : entity work.mem 
         generic map(
 			port_width => PORT_WIDTH,
 			block_count => BLOCK_COUNT,
@@ -40,6 +43,15 @@ begin
 			p1_addr => p1_addr, p2_addr => p2_addr,
 			p2_val_in => p2_val_in,
 			p1_val_out => p1_val_out, p2_val_out => p2_val_out);
+			
+	DUT_pc : entity work.program_counter
+	    generic map(pc_width => PORT_WIDTH)
+	    port map(clock => clock, 
+	             enable => '1',
+	             reset_n => reset_n_pc,
+	             load => '0',
+	             load_value => x"00000000",
+	             pc_value => pc_value_ext); 		
 
     gen_clk : process
 	begin
@@ -51,7 +63,8 @@ begin
 	
 	stimuli : process
 	begin
-		-- init
+		-- init (keep pc resetted)
+		reset_n_pc <= '0';
 		p1_enable <= '0';
 		p2_enable <= '0';
 		p2_write_enable <= '0';
@@ -60,7 +73,7 @@ begin
 		p2_val_in <= (others => '0');
 		wait for CLOCK_PERIOD;
 
-		-- read the first 32 addresses
+		-- read the first 32 addresses manually
 		p1_enable <= '1';
 		p2_enable <= '1';
 		for addr in 0 to 31 loop
@@ -69,12 +82,21 @@ begin
 			wait for CLOCK_PERIOD;
 		end loop;
 
-		-- write the first 33 addresses
+		-- write the first 33 addresses manually
 		p2_write_enable <= '1';
 		for addr in 0 to 31 loop
 			p1_addr <= ui2vec(addr, get_bit_count(BLOCK_COUNT));
 			p2_addr <= ui2vec(addr + 1, get_bit_count(BLOCK_COUNT));
 			p2_val_in <= ui2vec(vec2ui(x"DEADBEEF") + addr, PORT_WIDTH);
+			wait for CLOCK_PERIOD;
+		end loop;
+		
+	    -- read the first 32 addresses with the pc
+	    reset_n_pc <= '1';
+		p1_enable <= '1';
+		p2_enable <= '1';
+		for i in 0 to 31 loop
+			p1_addr <= pc_value_ext(10 downto 2);
 			wait for CLOCK_PERIOD;
 		end loop;
 		
