ebee2a502056462973ba533211acef450368c63d
CeDoMain <CeDoMain@users.noreply.github.com>
2023-06-14 14:47:52 +0200

    jump control should also calculate mem addresses

--- ex6_pipelined_cpu/src/3_execute_stage/addr_jump_control.vhd
+++ ex6_pipelined_cpu/src/3_execute_stage/addr_jump_control.vhd
@@ -2,7 +2,7 @@
 -- Company:     FAU Erlangen - Nuernberg
 -- Engineer:    Cedric Donges and Vittorio Serra
 --
--- Description: Calculates Jump/Branch targets and enables the jump if condition is met.
+-- Description: Calculates Address/Jump/Branch targets and enables the jump if condition is met.
 ----------------------------------------------------------------------------------
 
 library IEEE;
@@ -13,31 +13,32 @@ library work;
 use work.utils.ALL;
 use work.rv32i_defs.ALL;
 
-entity jump_control is
+entity addr_jump_control is
     Port(
         clock, reset_n, enable : IN std_logic;
         jump_condition : IN std_logic;
-        jump_base, jump_offset : IN std_logic_vector(xlen_range);
         jump_mode : IN jump_mode_type;
         jump_enable : OUT std_logic;
-        jump_target : OUT std_logic_vector(xlen_range));
-end jump_control;
 
-architecture bh of jump_control is
-    signal jump_base_reg : IN std_logic_vector(xlen_range);
-    signal jump_offset_reg : IN std_logic_vector(xlen_range);
-    signal jump_mode_reg : IN jump_mode_type;
+        addr_base, addr_offset : IN std_logic_vector(xlen_range);
+        addr_target : OUT std_logic_vector(xlen_range));
+end addr_jump_control;
+
+architecture bh of addr_jump_control is
+    signal addr_base_reg : std_logic_vector(xlen_range);
+    signal addr_offset_reg : std_logic_vector(xlen_range);
+    signal jump_mode_reg : jump_mode_type;
 begin
     INPUT_REGISTER : process (clock)
     begin
         if (rising_edge(clock)) then
             if (reset_n = '0') then
-                jump_base_reg <= (others => '0');
-                jump_offset_reg <= (others => '0');
+                addr_base_reg <= (others => '0');
+                addr_offset_reg <= (others => '0');
                 jump_mode_reg <= jump_none;
             elsif (enable = '1') then
-                jump_base_reg <= jump_base;
-                jump_offset_reg <= jump_offset;
+                addr_base_reg <= jump_base;
+                addr_offset_reg <= jump_offset;
                 jump_mode_reg <= jump_mode;
             end if;
         end if;
@@ -45,7 +46,7 @@ begin
     
     LOGIC : process (jump_base_reg, jump_offset_reg, jump_mode_reg, jump_condition)
     begin
-        jump_target <= std_logic_vector(unsigned(jump_base_reg) + unsigned(jump_offset_reg));
+        addr_target <= std_logic_vector(unsigned(addr_base_reg) + unsigned(addr_offset_reg));
         case jump_mode_reg is
             when jump_none =>
                 jump_enable <= '0';
