d4d46d33be5ed1a97ecee9dc60c3d3e76e046cb2
CeDoMain <CeDoMain@users.noreply.github.com>
2023-06-15 02:07:44 +0200

    add important notes

--- ex6_pipelined_cpu/src/x_global_stage/forwarding_control.vhd
+++ ex6_pipelined_cpu/src/x_global_stage/forwarding_control.vhd
@@ -11,3 +11,7 @@
 -- IN: rs1_value and rs2_value from wb stage
 -- OUT: rs1_value and rs2_value to dec stage
 -- OUT: rs1_select and rs2_select to wb stage
+
+-- THE NEWEST VALUE MUST BE FORWARDED!!!
+-- caution if forwarding values which comes from mem read
+-- 2 forwards, 1 stall path?
