ed2b65524bc7801bedfecd8b86ba1e8dc10d6f5d
CeDoMain <CeDoMain@users.noreply.github.com>
2023-07-16 15:37:23 +0200

    bugfix wrong counting

--- ex6_pipelined_cpu/src/peripherals/debouncer.vhd
+++ ex6_pipelined_cpu/src/peripherals/debouncer.vhd
@@ -2,10 +2,7 @@
 -- Company: FAU Erlangen - Nuernberg
 -- Engineer: Cedric Donges and Vittorio Serra
 --
--- Description: Debounce inputs.
---              if stable_count > 1: Let the signal through if it is stable for a given number of cycles.
---              if stable_count = 1: Let the signal through each cycle.
---              if stable_count = 0: Wire the signal through.
+-- Description: Let the signal through if the output would be stable for at least stable_count cycles.
 ----------------------------------------------------------------------------------
 
 library IEEE;
@@ -26,7 +23,7 @@ entity debouncer is
 end debouncer;
 
 architecture bh of debouncer is
-    constant counter_width : integer := get_bit_count(sel(stable_count > 0, stable_count, 1));
+    constant counter_width : integer := get_bit_count(sel(stable_count > 2, stable_count - 1, 2));
     type counter_type is array (port_width - 1 downto 0) of unsigned(counter_width - 1 downto 0);
 
     signal stage1, stage2 : std_logic_vector(port_width - 1 downto 0);
@@ -41,12 +38,10 @@ begin
                 for i in input'range loop
                     if (stage1(i) /= stage2(i)) then
                         counter(i) <= to_unsigned(0, counter_width);
+                    elsif (counter(i) = to_unsigned(stable_count - 2, counter_width)) then
+                        output(i) <= stage2(i);
                     else
                         counter(i) <= counter(i) + 1;
-                    end if;
-
-                    if (counter(i) = to_unsigned(stable_count, counter_width)) then
-                        output(i) <= stage2(i);
                     end if;
                 end loop;
             end if;
